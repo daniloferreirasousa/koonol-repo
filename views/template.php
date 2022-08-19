7f41d1780e9d99cd91910cc9dd63b163730820ee
DASF22 <daniloasf@outlook.com>
2022-08-18 22:50:53 -0300

    Banco de dados

--- views/template.php
+++ views/template.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL; ?>assets/css/style.css" />
 </head>
 <body>
+    <?=md5('1204@Dan.');?>
     <nav class="navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
